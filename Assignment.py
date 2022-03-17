57e909246b7482e99b170927e64ee058408ecd4b
Chidav <davidnwatarali@gmail.com>
2022-03-17 12:12:06 +0100

    my first commit

---
+++ Assignment.py
@@ -0,0 +1,17 @@
+# # write a code to find a leap year
+# year = int(input('Enter any year of your choice: '))
+# if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
+#     print(year, 'is a leap lear')
+# else:
+#     print(year, 'is not a leap year')
+
+# # write a code to find a century
+# year = int(input('Enter the year of your choice: '))
+# century = year / 100
+# cen = year // 100
+# if century == cen:
+#     print(int(century), 'century')
+# elif century > cen:
+#     print(cen + 1, 'century')
+# else:
+#     print('Error')
